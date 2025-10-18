f2af408926e6a789cb7b8f2abe6ff89a6764e342
andev03 <nguyenhoangan03.work@gmail.com>
2025-10-18 14:38:36 +0700

    fix: favicon.ic

--- src/main/java/com/loopus/loopus_be/config/WebConfig.java
+++ src/main/java/com/loopus/loopus_be/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.loopus.loopus_be.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,5 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:/var/www/uploads/");
     }
+
+    @Bean
+    ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
+
 }
 
